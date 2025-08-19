8eb7cb4e8d5f5615c31532dc5d54e29457be55ee
braju007 <erbrj7@gmail.com>
2025-08-19 12:11:13 +0530

    Import new components for children prop demo

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -3,6 +3,8 @@ import { Greet } from "./components/Greet";
 import { Person } from "./components/Person";
 import { PersonList } from "./components/PersonList";
 import { Status } from "./components/Status";
+import { Heading } from "./components/Heading";
+import { Oscar } from "./components/Oscar";
 
 function App() {
   const personName = {
@@ -30,6 +32,10 @@ function App() {
       <Person name={personName} />
       <PersonList names={nameList} />
       <Status status="success" />
+      <Heading>Hello! Raju Juganavar</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Raju Juganavar!</Heading>
+      </Oscar>
     </div>
   );
 }
