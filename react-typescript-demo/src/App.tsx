bdfc1ccc8f2eeb51677de4925ec4b3d40c780a8a
braju007 <erbrj7@gmail.com>
2025-08-19 11:48:41 +0530

    Import components with types in App

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -1,10 +1,33 @@
 import "./App.css";
 import { Greet } from "./components/Greet";
+import { Person } from "./components/Person";
+import { PersonList } from "./components/PersonList";
 
 function App() {
+  const personName = {
+    first: "Basavaraj",
+    last: "Juganavar",
+  };
+
+  const nameList = [
+    {
+      first: "Raju",
+      last: "Juganavar",
+    },
+    {
+      first: "Sonu",
+      last: "Raj",
+    },
+    {
+      first: "Rishu",
+      last: "Raj",
+    },
+  ];
   return (
     <div className="App">
-      <Greet name="Raju" unreadMessages={10} />
+      <Greet name="Raju" unreadMessages={10} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
